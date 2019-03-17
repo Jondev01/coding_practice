76be51de6908ec6e8c544010cd0004639b565d90
Jonathan Wernersson <jonathan2008@live.se>
2019-03-17 15:19:32 +0100

    added dp solution to 38 faster than 100%

---
+++ Leetcode/38. Count and Say/non-recursive.php
@@ -0,0 +1,34 @@
+<?php 
+
+//https://leetcode.com/problems/count-and-say/
+
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return String
+     */
+    function countAndSay($n) {
+        $dp = array();
+        $dp[1] = "1";
+        for($i=2; $i<=$n; ++$i) {
+            $last = $dp[$i-1];
+            $length = strlen($last);
+            $res = "";
+            for($curPos = 0; $curPos <$length; ++$curPos){
+                $curChar = $last[$curPos];
+                $count = 0;
+                while($curChar == $last[$curPos]) {
+                    $count++;
+                    $curPos++;
+                }
+                $res .= "$count".$curChar;
+                $curPos--;
+            }
+            $dp[i] = $res;
+        }
+        return $dp[$n];
+    }
+}
+
+?>
